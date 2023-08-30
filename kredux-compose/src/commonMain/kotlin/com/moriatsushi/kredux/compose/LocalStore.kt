d70fc7ff1cb178254ad3d2515e0373abc0064a71
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-08-30 21:04:04 +0900

    Add LocalStore

---
+++ kredux-compose/src/commonMain/kotlin/com/moriatsushi/kredux/compose/LocalStore.kt
@@ -0,0 +1,47 @@
+package com.moriatsushi.kredux.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidedValue
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.moriatsushi.kredux.Store
+
+interface LocalStore<T : Store<*, *>> {
+    @get:Composable
+    val current: T
+
+    infix fun provides(value: T): ProvidedValue<T>
+}
+
+fun <T : Store<*, *>> localStoreOf(): LocalStore<T> {
+    return LocalStoreImpl()
+}
+
+private class LocalStoreImpl<T : Store<*, *>> : LocalStore<T> {
+    override val current: T
+        @Composable
+        get() = local.current
+
+    override fun provides(value: T): ProvidedValue<T> {
+        return local provides value
+    }
+
+    private val local = staticCompositionLocalOf<T> {
+        error("No store provided")
+    }
+}
+
+@Composable
+fun <T : Store<S, *>, S, R> LocalStore<T>.select(selector: (S) -> R): State<R> {
+    val state by current.state.collectAsState()
+    return derivedStateOf { selector(state) }
+}
+
+@Composable
+fun <T : Store<*, Action>, Action> LocalStore<T>.dispatch(action: Action) {
+    val store = current
+    store.dispatch(action)
+}
